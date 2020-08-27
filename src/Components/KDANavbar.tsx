cc28ce56f7f5cfa9a11fbec4078980394413b160
Carlos Cerna <cbcerna@ksu.edu>
2020-08-26 21:04:35 -0500

    Material-ui added, changed packages to suit

---
+++ src/Components/KDANavbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+export class KDANavbar extends React.Component {
+  render() {
+    return <Button color="default">Login</Button>;
+  }
+}
