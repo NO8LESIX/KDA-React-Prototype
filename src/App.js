51475f0dfac8d6e4b31db50fe7a72c58caf1c4a4
benjaminh1234 <benjaminh1234@yahoo.com>
2020-08-28 17:54:40 -0500

    updates on report page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AboutPage from "./Pages/AboutPage";
 import SNAPInfoPage from "./Pages/SNAPInfoPage";
 import ProducePage from "./Pages/ProducePage";
+import FileReportPage from "./Pages/FileReportPage";
 
 export default function App() {
   return (
@@ -34,6 +35,9 @@ export default function App() {
             <Route path="/produce">
               <Produce />
             </Route>
+            <Route path="/report">
+                <FileReport />
+            </Route>
           </Switch>
         </div>
       </Router>
@@ -58,6 +62,10 @@ function SNAPInfo() {
 
 function Produce() {
   return <ProducePage />;
+}
+
+function FileReport() {
+    return <FileReportPage />;
 }
 
 function ReactDefault() {
