ba67fa2cefe13134b4d557acdb0209de293af89e
Jasmine Hunt <jmhunt29@ksu.edu>
2020-08-31 11:32:31 -0500

    Revert "Added Borders for middle cards."
    This reverts commit 05b04f3f70b242181d683b64eba98bd30844636f.

--- src/Pages/FileReportPage.tsx
+++ src/Pages/FileReportPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography, Link, Box} from "@material-ui/core";
+import { Grid, Typography, Link } from "@material-ui/core";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
 import KDANavbar from "../Components/KDANavbar";
@@ -17,9 +17,8 @@ export default class FileReportPage extends React.Component {
                             <Typography align="left" variant="h6"> --Did you attend an event where people became ill?</Typography>
                             <Typography align="left" variant="h6"> --Is there a problem with a restaurant you visited or food item you purchased?</Typography>
                             <Typography align="left" variant="h6">Kansas Department of Health & Environment and Kansas Department of Agriculture work cooperatively to investigate foodborne illness outbreaks. </Typography>
-                    </Grid>
-                    <Grid item xs={6} >
-                        <Box display="flex" flexDirection="column" justifyContent="center" border={2} padding={2}>
+                        </Grid>
+                        <Grid item xs={6}>
                             <Typography align="left" variant="h4"> Report illness caused by a restaurant, food item, or event </Typography>
                             <Typography align="center" variant="h6"> -Call 877-427-7317  </Typography>
                             <Typography align="center">
@@ -27,10 +26,8 @@ export default class FileReportPage extends React.Component {
                                     File An Online Report
                                     </Link>
                             </Typography>
-                        </Box>
                         </Grid>
-                    <Grid item xs={6} >
-                        <Box display="flex" flexDirection="column" justifyContent="center" border={2} padding={2}>
+                        <Grid item xs={6}>
                                 <Typography align="left" variant="h4"> Report a problem with a restaurant or food that did NOT cause illness </Typography>
                                 <Typography align="center">
                                     <Link align="center" href="https://agriculture.ks.gov/kda-services/complaint-form/food-safety-and-lodging-complaint">
@@ -39,7 +36,6 @@ export default class FileReportPage extends React.Component {
                                 </Typography>
                                 <Typography align="center" variant="h6"> -Email kda.fsl@ks.gov</Typography>
                                 <Typography align="center" variant="h6"> -Or call 785-564-6767</Typography>
-                        </Box>
                         </Grid>
                         <Grid item xs={12}>
                             <Typography align="center" variant="h4"> Why It's Important to Report Food Poisoning </Typography>
