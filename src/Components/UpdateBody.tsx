454d83d77e50f0499e2f64692f7741f547841455
Carlos Cerna <cbcerna@ksu.edu>
2020-09-01 17:07:00 -0500

    Update UpdateBody.tsx

--- src/Components/UpdateBody.tsx
+++ src/Components/UpdateBody.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { styled } from "@material-ui/core/styles";
-import clsx from 'clsx';
-import Grid from "@material-ui/core/Grid";
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
@@ -11,7 +7,6 @@ import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from "@material-ui/core/IconButton";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { TestUpdates } from "../Data/TestUpdates";
 import { Typography, Box } from '@material-ui/core';
 import "../CSS/UpdateBody.css";
